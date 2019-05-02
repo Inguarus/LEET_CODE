d2de48c0ea9d92225371899970edcccc4eb53980
Inguarus <inguarus87@gmail.com>
2019-05-02 04:42:25 +0300

    second commit

---
+++ src/com/inguarus/ReverseWords.java
@@ -0,0 +1,24 @@
+package com.inguarus;
+
+/*
+  Given a string, you need to reverse the order of characters in each word
+within a sentence while still preserving whitespace and initial word order.
+  Note: In the string, each word is separated by single space and there will
+not be any extra space in the string.
+ */
+
+public class ReverseWords {
+    public static void main(String[] args) {
+    }
+
+    public static String reverseWords(String s) {
+        String[] words = s.split(" ");
+        StringBuilder reverse = new StringBuilder();
+        for (int i = 0; i < words.length; i++) {
+            if (i > 0)
+                reverse.append(" ");
+            reverse.append(new StringBuilder(words[i]).reverse());
+        }
+        return reverse.toString();
+    }
+}
---
+++ src/com/inguarus/SortedSquares.java
@@ -0,0 +1,22 @@
+package com.inguarus;
+
+/*
+Given an array of integers A sorted in non-decreasing order,
+return an array of the squares of each number, also in sorted non-decreasing order.
+ */
+
+import java.util.Arrays;
+
+public class SortedSquares {
+    public static void main(String[] args) {
+    }
+
+    public int[] sortedSquares(int[] A) {
+        int[] resultSquare = new int[A.length];
+        for (int i = 0; i < A.length; i++) {
+            resultSquare[i] = A[i] * A[i];
+        }
+        Arrays.sort(resultSquare);
+        return resultSquare;
+    }
+}
